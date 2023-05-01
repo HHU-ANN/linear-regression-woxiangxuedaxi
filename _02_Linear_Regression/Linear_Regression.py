8804453f92cf97710aef5536680e8d376db9d9a4
DING <277909353@qq.com>
2023-05-01 17:45:32 +0800

    第一次

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -9,12 +9,24 @@ except ImportError as e:
     import numpy as np
 
 def ridge(data):
-    pass
-    
+    x,y = read_data()
+    t = 0.1
+    weight = np.matmul(np.linal.inv(np.matmul(x.T,x)+t*np.eye()),np.matmul(x.T,y))
+    return weight @ data
+
+
 def lasso(data):
-    pass
+    a = 0.01
+    t = 0.1
+    x,y = read_data()
+    min_max_scaler = preprocessing.MinMaxScaler()
+    x = min_max_scaler.fit_transform(x)
+    y = min_max_scaler.fit_transform(y)
+    weight = weight - a*np.matmul(np.linal.inv(np.matmul(x.T,x)),np.matmul(x.T,y)-t/2)
+    return weight @ data
 
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
     y = np.load(path + 'y_train.npy')
     return x, y
+
