2f93f1eb305ea5f7df8f67d29b50e09cb05fa57b
DING <277909353@qq.com>
2023-05-03 10:12:47 +0800

    7

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -23,14 +23,14 @@ def lasso(data):
     a = 0.07
     t = 0.05
     x, y = read_data()
-    global wei
-    wei = 0.01
+    wei = np.array([1,1,1,1,1,1])
     # min_max_scaler = preprocessing.MinMaxScaler()
     # x = min_max_scaler.fit_transform(x)
     # y = min_max_scaler.fit_transform(y)
     wei = wei - a * np.matmul(np.linalg.inv(np.matmul(x.T, x)), np.matmul(x.T, y) - t / 2)
     return wei @ data
 
+    
 
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
