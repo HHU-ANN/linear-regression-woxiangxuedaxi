47de779bdb1dc6ceadbe3cf91b5d7fc99d55f426
DING <277909353@qq.com>
2023-05-05 19:56:45 +0800

    123123

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -14,7 +14,7 @@ except ImportError as e:
 
 def ridge(data):
     x, y = read_data()
-    t = 0.08
+    t = -0.00001
     weight = np.matmul(np.linalg.inv(np.matmul(x.T, x) + t * np.eye(6)), np.matmul(x.T, y))
     return weight @ data
 
