c1ac9f4f41d10bff67b3c6794803f55eba0647a9
DING <98734433+woxiangxuedaxi@users.noreply.github.com>
2023-05-05 20:48:10 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -22,7 +22,7 @@ def ridge(data):
 def lasso(data):
     a = 1
     t = -0.1001
-    u =0.0333
+    u =0.00000000333
     x, y = read_data()
     wei = np.array([1, 1, 1, 1, 1, 1])
     # min_max_scaler = preprocessing.MinMaxScaler()
