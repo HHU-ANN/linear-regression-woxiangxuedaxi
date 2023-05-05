f17e5b647c87fdae72c25e1bcf8b6b1eea0c0d03
DING <98734433+woxiangxuedaxi@users.noreply.github.com>
2023-05-05 20:36:53 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -21,13 +21,13 @@ def ridge(data):
 
 def lasso(data):
     a = 1
-    t = 0.1
+    t = -0.1001
     x, y = read_data()
     wei = np.array([1, 1, 1, 1, 1, 1])
     # min_max_scaler = preprocessing.MinMaxScaler()
     # x = min_max_scaler.fit_transform(x)
     # y = min_max_scaler.fit_transform(y)
-    wei = a * np.matmul(np.linalg.inv(np.matmul(x.T, x)), (np.matmul(x.T, y) -  np.dot(t * wei)))
+    wei = a * np.matmul(np.linalg.inv(np.matmul(x.T, x)), np.matmul(x.T, y) -  t * wei)
     return wei @ data
 
 
